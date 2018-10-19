8de832a8eddd94acee615003f8ea2a437734c304
Daniele Sabre <daniele.sabre@gmail.com>
2018-10-19 17:14:33 +0200

    Develop

--- start.sh
+++ start.sh
@@ -5,10 +5,26 @@ declare -A APPENVS
 # base envs
 APPENVS[APP_NAME]=test_container
 APPENVS[APP_PATH]=./app/
-APPENVS[EXTERNAL_WEB_PORT]=8315
+APPENVS[IP_COUNTER]=2 # 2-254 (or 'auto')
+
+if [ ${APPENVS["IP_COUNTER"]} == 'auto' ]; then
+  echo 'TODO'
+  exit 1
+  function getNextIp() {
+    ping -c 1 -W 1 172.20.0.$1 > /dev/null
+
+    if [ $? -eq 0 ]; then
+      echo $(getNextIp `expr ${1} + 1`)
+    else
+      echo $1
+    fi
+  }
+
+  APPENVS[IP_COUNTER]=$(getNextIp 2)
+fi
 
 # database envs
-APPENVS[EXTERNAL_DB_PORT]=9906
+APPENVS[EXTERNAL_DB_PORT]=`expr ${APPENVS["IP_COUNTER"]} + 9000`
 APPENVS[MYSQL_ROOT_PASSWORD]=my_secret_pw_shh
 APPENVS[MYSQL_DATABASE]=test_db
 APPENVS[MYSQL_USER]=devuser
@@ -33,4 +49,14 @@ echo "Launch ${APP_NAME}"
 echo
 docker-compose up -d
 
-exit $?
+# check if dockers are started
+if [ ! $? -eq 0 ]; then
+    exit $?
+fi
+
+# print web app link
+echo
+echo "LINK: http://172.20.0.${APPENVS["IP_COUNTER"]}:8000"
+echo
+
+exit 0
